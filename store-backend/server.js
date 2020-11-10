ead62cb4edb739992968bf1c5764a6c1040341c0
Pawel L <virtuozoo@o2.pl>
2020-11-10 16:45:15 +0100

    remove mongoose

--- store-backend/server.js
+++ store-backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express'
-import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
 
 const app = express();
-const port = 8000;
+const port = process.dotenv || 8000;
 
+
+
+
+// api endpoints
 app.get('/', (req, res) => res.status(200).send('hello world'));
-
-
+//listen
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
