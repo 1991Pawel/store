2d8a877e10e136e8b0695229bee5ca80c816abdf
Pawel L <virtuozoo@o2.pl>
2020-11-13 20:06:36 +0100

    rename component

--- store-frontend/components/NewCollection.tsx
+++ store-frontend/components/NewCollection.tsx
@@ -1,24 +1,26 @@
-import styles from '../styles/BestSellers.module.scss'
+import styles from '../styles/NewCollection.module.scss'
+import SvgCartIcon from './SvgCartIcon'
+import SvgHeartIcon from './SvgHeartIcon'
 
-const BestSellers = () => (
+const NewCollection = () => (
     <section className={styles.best}>
         <div className="wrapper">
             <h2>BestSellers</h2>
             <ul className={styles.best__items}>
                 <li className={styles.best__item}>
-                    <p className={styles.best__title}>description</p>
+                    <button className={styles.best__btn}>discover</button>
                     <img src="https://img01.ztat.net/article/spp-media-p1/4e5c287bfcb332fbbb2bd4194f0775a8/aef9d23778ed46a99994a27fe8bdd99a.jpg?imwidth=762" alt="" />
                 </li>
                 <li className={styles.best__item}>
-                    <p className={styles.best__title}>description</p>
-                    <img src="https://img01.ztat.net/article/spp-media-p1/7c2d7afa446a332aa033dbd4e42af10f/092fc816b6df4b688940012abb241c53.jpg?imwidth=1800" alt="" />
+                    <button className={styles.best__btn}>discover</button>
+                    <img src="https://img01.ztat.net/article/spp-media-p1/c6afcbbdf2173c5da83cb65665ee2236/ffa99fb038674d0e92c2655ddb620333.jpg?imwidth=1800" alt="" />
                 </li>
                 <li className={styles.best__item}>
-                    <p className={styles.best__title}>description</p>
+                    <button className={styles.best__btn}>discover</button>
                     <img src="https://img01.ztat.net/article/spp-media-p1/abef8788b9273812951dd4db48b11ac7/e90e74d7b04c4caaa493608422c6ec6f.jpg?imwidth=1800" alt="" />
                 </li>
                 <li className={styles.best__item}>
-                    <p className={styles.best__title}>description</p>
+                    <button className={styles.best__btn}>discover</button>
                     <img src="https://img01.ztat.net/article/spp-media-p1/7c2d7afa446a332aa033dbd4e42af10f/092fc816b6df4b688940012abb241c53.jpg?imwidth=1800" alt="" />
                 </li>
             </ul>
@@ -26,4 +28,4 @@ const BestSellers = () => (
     </section>
 )
 
-export default BestSellers;
+export default NewCollection;
