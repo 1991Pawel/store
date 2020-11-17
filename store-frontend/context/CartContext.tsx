f6f70f5fbe0436236a1cb86bec70008a75deaa43
Pawel L <virtuozoo@o2.pl>
2020-11-18 00:00:14 +0100

    add eslint

--- store-frontend/context/CartContext.tsx
+++ store-frontend/context/CartContext.tsx
@@ -1,19 +1,23 @@
-import React, { createContext, useState } from 'react'
-import { Product } from '../types/type'
+import React, { createContext, useState } from 'react';
+import { Product } from '../types/type';
 
 export const CartContext = createContext({});
 
 const CartProvider = ({ children, data }) => {
-    const [cartItems, setCartItems] = useState(data);
+  const [cartItems, setCartItems] = useState(data);
 
-    const addItemToCart = (product: Product) => setCartItems([...cartItems, product]);
-    const removeItemFromCart = (id: number) => setCartItems(cartItems.filter((item) => item.id !== id));
+  const addItemToCart = (product: Product) =>
+    setCartItems([...cartItems, product]);
+  const removeItemFromCart = (id: number) =>
+    setCartItems(cartItems.filter((item) => item.id !== id));
 
-    return (
-        <CartContext.Provider value={{ cartItems, addItemToCart, removeItemFromCart }}>
-            {children}
-        </CartContext.Provider>
-    )
-}
+  return (
+    <CartContext.Provider
+      value={{ cartItems, addItemToCart, removeItemFromCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
 
 export default CartProvider;
